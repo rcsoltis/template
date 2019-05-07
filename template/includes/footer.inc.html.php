b34a8e8a3b275782a2f19e14b35631089001b449
RC Soltis <48971032+rcsoltis@users.noreply.github.com>
2019-05-07 13:04:35 -0400

    Add files via upload
    Changed the weather widget background color to white.
    Reversed the background/text colors for a few sections in order for the page to be lighter in color.
    Added some red border accents.

--- template/includes/footer.inc.html.php
+++ template/includes/footer.inc.html.php
@@ -1,42 +1,40 @@
  <footer>
-        <h3>Header (h3)</h3>
+     <h3>Header (h3)</h3>
 
-        <form id="contact" action="">
-            <label>First name:</label><input type="text" name="fname">
-            <label>Last name:</label><input type="text" name="lname">
-            <label>Question or Comment?</label><textarea rows="6"></textarea><br>
-            <label>Role:</label>
-            <label>(select)</label><input type="radio"><br>
-            <input type="submit" value="Submit">
-        </form>
-        <div class="section group">
-            <aside class="col span_4_of_12">
-                <div class="fb-page" data-href="https://www.facebook.com/cas222cascade/" data-tabs="timeline" data-width="" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-                    <blockquote cite="https://www.facebook.com/cas222cascade/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/cas222cascade/">CAS 222</a></blockquote>
-                </div>
-            </aside><br>
-            <aside class="col span_4_of_12">
-                <a class="twitter-timeline" data-width="340" data-height="500" href="https://twitter.com/pcccas222?ref_src=twsrc%5Etfw">Tweets by pcccas222</a>
-                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-            </aside>
-            <aside class="col span_4_of_12">
-                <a class="weatherwidget-io" href="https://forecast7.com/en/45d52n122d68/portland/?unit=us" data-label_1="PORTLAND" data-label_2="WEATHER" data-theme="original">PORTLAND WEATHER</a>
-                <script>
-                    ! function(d, s, id) {
-                        var js, fjs = d.getElementsByTagName(s)[0];
-                        if (!d.getElementById(id)) {
-                            js = d.createElement(s);
-                            js.id = id;
-                            js.src = 'https://weatherwidget.io/js/widget.min.js';
-                            fjs.parentNode.insertBefore(js, fjs);
-                        }
-                    }(document, 'script', 'weatherwidget-io-js');
+     <form id="contact" action="">
+         <label>First name:</label><input type="text" name="fname">
+         <label>Last name:</label><input type="text" name="lname">
+         <label>Question or Comment?</label><textarea rows="6"></textarea><br>
+         <label>Role:</label>
+         <label>(select)</label><input type="radio"><br>
+         <input type="submit" value="Submit">
+     </form>
+     <div class="section group">
+         <aside class="col span_4_of_12">
+             <div class="fb-page" data-href="https://www.facebook.com/cas222cascade/" data-tabs="timeline" data-width="" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                 <blockquote cite="https://www.facebook.com/cas222cascade/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/cas222cascade/">CAS 222</a></blockquote>
+             </div>
+         </aside><br>
+         <aside class="col span_4_of_12">
+             <a class="twitter-timeline" data-width="340" data-height="500" href="https://twitter.com/pcccas222?ref_src=twsrc%5Etfw">Tweets by pcccas222</a>
+             <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+         </aside>
+         <aside class="col span_4_of_12">
+             <a class="weatherwidget-io" href="https://forecast7.com/en/45d52n122d68/portland/?unit=us" data-label_1="PORTLAND" data-label_2="WEATHER" data-theme="original" data-basecolor="#ffff" data-shadow="#f7f3f3" data-textcolor="#000000" data-highcolor="#9d0d0d" data-lowcolor="#06306c" data-mooncolor="#0657b8" data-cloudcolor="#2f2d2d" data-cloudfill="#f7f9fa" data-raincolor="#0569f9" data-snowcolor="#060606">PORTLAND WEATHER</a>
+             <script>
+                 ! function(d, s, id) {
+                     var js, fjs = d.getElementsByTagName(s)[0];
+                     if (!d.getElementById(id)) {
+                         js = d.createElement(s);
+                         js.id = id;
+                         js.src = 'https://weatherwidget.io/js/widget.min.js';
+                         fjs.parentNode.insertBefore(js, fjs);
+                     }
+                 }(document, 'script', 'weatherwidget-io-js');
 
-                </script>
-                <p>Copyright&copy;<?php echo date("Y");?> Ace in the Hole</p>
-                <img src="images/Triathlon_all_3_stages_pictogram.svg" alt="triathlon logo" height="160px" width="160px">
-            </aside>
-        </div>
-    </footer>
-
- 
+             </script>
+             <p>Copyright&copy;<?php echo date("Y");?> Ace in the Hole</p>
+             <img src="images/Triathlon_all_3_stages_pictogram.svg" alt="triathlon logo" height="160px" width="160px">
+         </aside>
+     </div>
+ </footer>
